e879347d2296e8c9d702d007d3027686dc0f52ad
stefinfection <sjeangeo@gmail.com>
2016-05-24 16:37:36 -0600

    Created MVC skeleton

---
+++ Version_1/Version_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Version_1.Startup))]
+namespace Version_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
